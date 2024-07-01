cde3c7c4be7b372336c3517aec2f4ffa5f1b916c
zatway <radmirar00@mail.ru>
2024-07-01 22:44:30 +0500

    перенос файлов в папки

--- BookCatalog/Views/MainWindow.xaml.cs
+++ BookCatalog/Views/MainWindow.xaml.cs
@@ -10,7 +10,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace BookCatalog
+namespace BookCatalog.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
